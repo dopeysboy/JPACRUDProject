e4ba213e5ed8bc16e08d3be5d41004404b7cdb35
Tyler Tanner <tylertanner@Tylers-MacBook-Air.local>
2022-10-14 17:36:36 -0500

    Completed boilerplate

---
+++ CampaignTracker/src/main/java/com/skilldistillery/dnd/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package com.skilldistillery.dnd.controllers;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping(path= {"/", "index.do"})
+	public String index(HttpSession session) {
+		return "index";
+	}
+}
